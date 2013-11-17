f5262e22044af552c55b9bbd0994c768e51ccf7f
horknox <horknox@gmail.com>
2013-11-18 05:59:01 +0900

    add sample page

---
+++ unni/WebContent/view/home/front.jsp
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Welcome to Spring Web MVC project</title>
+    </head>
+
+    <body>
+        <p>Hello! This is the default welcome page for a Spring Web MVC project.</p>
+        <a href ="HelloWorldPage.htm">Hello</a>
+    </body>
+</html>
---
+++ unni/src/org/sparcs/unni/HomeController.java
@@ -0,0 +1,19 @@
+package org.sparcs.unni;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public ModelAndView frontPage() throws Exception {
+		ModelAndView mav = new ModelAndView();
+
+		mav.addObject("user", "WOW");
+		mav.setViewName("front");
+		return mav;
+	}
+
+}
